c4a675cdfcb541a97107cf617ef92f2b827e35ab
zlef <me@zacharylefevre.com>
2025-01-20 22:41:03 -0600

    compiling

--- czox/scanner.c
+++ czox/scanner.c
@@ -87,12 +87,97 @@ void skipWhitespace() {
   }
 }
 
+Token string() {
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') scanner.line++;
+    advance();
+  }
+  if (isAtEnd()) return makeError("Unterminated String");
+  // move past the closing "
+  advance();
+  return makeToken(TOKEN_STRING);
+}
+
+bool isDigit(char c) {
+  return c >= '0' && c <= '9';
+}
+
+bool isAlpha(char c) {
+  return c >= 'a' && c <= 'z' ||
+    c >= 'A' && c <= 'Z' ||
+    c == '_';
+}
+
+static TokenType checkKeyword(int start, int length,
+                              const char* rest, TokenType type) {
+  if (scanner.current - scanner.start == start + length &&
+      memcmp(scanner.start + start, rest, length) == 0) {
+    return type;
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+
+TokenType identifierType() {
+  switch (scanner.start[0]) {
+  case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+  case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+  case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+  case 'f':
+    if (scanner.current - scanner.start > 1) {
+      switch (scanner.start[1]) {
+      case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+      case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+      case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+      }
+    }
+    break;
+  case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+  case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+  case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+  case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+  case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+  case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+  case 't':
+    if (scanner.current - scanner.start > 1) {
+      switch (scanner.start[1]) {
+      case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+      case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+      }
+    }
+    break;
+  case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+  case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek())) advance();
+  makeToken(identifierType());
+}
+
+Token number() {
+  while (isDigit(peek())) advance();
+  if (peek() == '.' && isDigit(peekNext())) {
+    advance();
+    while (isDigit(peek())) advance();
+  }
+  return makeToken(TOKEN_NUMBER);
+}
+
 Token scanToken() {
   skipWhitespace();
   scanner.start = scanner.current;
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
   char c = advance();
+
+  if(isAlpha(c)) return identifier();
+  if(isDigit(c)) return number();
+
 
   switch (c) {
   case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -110,6 +195,7 @@ Token scanToken() {
   case '=': return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
   case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
   case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+  case '"': return string();
   }
   return makeError("Unexpected Character");
 }
--- czox/vm.c
+++ czox/vm.c
@@ -81,6 +81,19 @@ InterpretResult run() {
 }
 
 InterpretResult interpret(const char* source) {
-  compile(source);
-  return INTERPRET_OK;
+  Chunk chunk;
+  initChunk(&chunk);
+
+  if (!compile(source, &chunk)) {
+    freeChunk(&chunk);
+    return INTERPRET_COMPILE_ERROR;
+  }
+  vm.chunks = &chunk;
+  vm.ip = vm.chunks->code;
+
+  InterpretResult result = run();
+
+  freeChunk(&chunk);
+
+  return result;
 }
